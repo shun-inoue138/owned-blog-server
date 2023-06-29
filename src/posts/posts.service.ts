ccf37e5eded8c569f8e76bd8724b8f13fd10604e
shun-inoue138 <57622411+shun-inoue138@users.noreply.github.com>
2023-06-29 18:18:46 +0900

    add:postsとusersのserviceメソッドを仮作成

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,33 @@
+import { UsersService } from './../users/users.service';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Post, PostDocument } from './schema';
+import { Model } from 'mongoose';
+import { CreatePostDto } from './dto/create-post.dto';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(
+    @InjectModel(Post.name) private postModel: Model<Post>,
+    private usersService: UsersService,
+  ) {}
+
+  async create(createPostDto: CreatePostDto): Promise<PostDocument> {
+    const postToCreate = new this.postModel(createPostDto);
+    const res = await postToCreate.save();
+
+    const user = await this.usersService.getUser(createPostDto.user);
+    user.posts.push(postToCreate);
+    await user.save();
+
+    return res;
+  }
+
+  async findAll(): Promise<PostDocument[]> {
+    return this.postModel.find().populate('user').exec();
+  }
+
+  async findOne(id: string): Promise<PostDocument> {
+    return this.postModel.findById(id).populate('user').exec();
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  getUsers(): Promise<UserDocument[]> {
+    return this.userModel.find().exec();
+  }
+  getUser(id): Promise<UserDocument> {
+    return this.userModel.findById(id).exec();
+  }
+
+  createUser(): Promise<UserDocument> {
+    const userToCreate = new this.userModel({
+      name: 'test1',
+      email: 'hoge@fuga.com',
+      password: '1234',
+    });
+    return userToCreate.save();
+  }
+}
