eed680f0cebddfc6442a91987b6c44586f10a9e0
shun-inoue138 <57622411+shun-inoue138@users.noreply.github.com>
2023-06-30 11:48:53 +0900

    mod:moduleエラー解消

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Post, PostSchema } from './schema';
-import { User, UserSchema } from 'src/users/schema';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
-
+import { UsersModule } from 'src/users/users.module';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]),
+    UsersModule,
   ],
   controllers: [PostsController],
   providers: [PostsService],
